c3398e86490ea2001a0990fc6546aab084a89132
Mickaël Bannholtzer <bannholtzer.mickael@gmail.com>
2022-12-17 00:22:50 +0100

    je me suis arreté au 2.5 du front, faut débug

--- quiz-ui/src/router/index.js
+++ quiz-ui/src/router/index.js
@@ -1,14 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomePage from '../views/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "HomePage",
+      component: HomePage,
     },
+    // ... autres routes
     {
       path: '/about',
       name: 'about',
