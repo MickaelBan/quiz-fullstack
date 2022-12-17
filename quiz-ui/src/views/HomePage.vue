fca6409b22426fd340360c9d3e30877da04f968a
mickael bannholtzer <bannholtzer.mickael@gmail.com>
2022-12-17 19:45:40 +0100

    homepage 2.7a

--- quiz-ui/src/views/HomePage.vue
+++ quiz-ui/src/views/HomePage.vue
@@ -1,17 +1,23 @@
 <template>
   <h1>Home page</h1>
+
+  <div v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
+    {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
+  </div>
 </template>
 
 <script>
-// import quizApiService from "@/services/QuizApiService";
+import quizApiService from "@/services/QuizApiService";
 
 export default {
   name: "HomePage",
   data() {
     return {
+      registeredScores: []
     };
   },
   async created() {
+    this.registeredScores = await quizApiService.getQuizInfo()
     console.log("Composant Home page 'created'");
   }
 };
