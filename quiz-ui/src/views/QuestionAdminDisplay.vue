e554e893107b7e1642ca8de89f357a4ba888f139
Samy Abdouche <samy.abdouche@gmail.com>
2023-01-06 19:35:03 +0100

    I hate props

---
+++ quiz-ui/src/views/QuestionAdminDisplay.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div class="button" id="edit">Editer la question</div>
+    <div class="button" id="delete">Supprimer la question</div>
+
+    <div class="question">
+        <!-- <a>{{ question.title }}</a> -->
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: "QuestionAdminDisplay", 
+    props: ['question'],
+    mounted(){
+        console.log(this.question);
+    },
+    methods: {
+        handler(question){
+            console.log(question);
+        }
+    }
+}
+</script>
