b7aac8eb558871b4e9c572fee03806c1fa09bd8b
Samy Abdouche <samy.abdouche@gmail.com>
2023-01-06 15:45:08 +0100

    Login page

---
+++ quiz-ui/src/views/Login.vue
@@ -0,0 +1,50 @@
+
+<template>
+
+    <div id="login" v-if="isToken">
+
+        <label for="login">Mot de passe </label>
+        <input type="password" v-model="pwd"/>
+        <div class="button">
+            <input type="submit" name="login" value="Connexion" v-on:click="login(pwd)">
+        </div>
+    </div>
+
+    <div id="back-office" v-else>
+        <p>Bien joué bg</p>
+    </div>
+
+</template>
+
+<style>
+</style>
+
+<script>
+import QuizApiService from "../services/QuizApiService";
+
+
+
+export default {
+    name: "Login",
+
+    data(){
+        let adminMode = false;
+        return(adminMode);
+    },
+    methods: {
+        login(pwd){
+            let response = QuizApiService.postLogin(pwd)
+            console.log(response)
+        }
+    },
+    computed: {
+        isToken(){
+            let token = window.localStorage.getItem("token");
+            return (token === null ? true : false);
+        }
+    }
+        
+    
+}
+</script>
+
