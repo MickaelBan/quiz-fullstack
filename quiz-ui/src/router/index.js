616317db013d29c21fc09c31213bd988b4674cc2
Samy Abdouche <samy.abdouche@gmail.com>
2023-01-06 16:55:29 +0100

    added corect route for login

--- quiz-ui/src/router/index.js
+++ quiz-ui/src/router/index.js
@@ -3,6 +3,7 @@ import HomePage from '../views/HomePage.vue'
 import NewQuizPage from '../views/NewQuizPage.vue'
 import QuestionsManager from '../components/QuestionsManager.vue'
 import ScoresPage from '../views/ScoresPage.vue'
+import Login from '../views/ScoresPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: '/scores',
       name: 'Scores',
       component: ScoresPage
+    },
+    {
+      path: '/login',
+      name: 'Login', 
+      component: Login
     }
   ]
 })
